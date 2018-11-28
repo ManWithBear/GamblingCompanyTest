d4a6782a0e6faa23e4521a7faedbbcdeb0038a98
Bogomolov Denis <denis.bogomolov.nsk@yandex.ru>
2018-11-28 23:29:45 +0100

    Initial commit

---
+++ UIBuilder/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  UIBuilder
+//
+//  Created by Denis Bogomolov on 28/11/2018.
+//  Copyright © 2018 bdk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController { }
